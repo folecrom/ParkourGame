9c12dcdfd122087db13a51491a0ea81b668e631b
folecrom <90184290+folecrom@users.noreply.github.com>
2023-03-03 16:13:08 +0100

    jump

--- Parkour Game/Assets/script/PlayerController.cs
+++ Parkour Game/Assets/script/PlayerController.cs
@@ -8,7 +8,10 @@ public class PlayerController : MonoBehaviour
     private CharacterController _characterController;
 
     private Vector3 _direction;
-
+    private float _velocity;
+    private float _gravity = -9.81f;
+    [SerializeField] private float gravityMultiplier = 1.0f;
+    [SerializeField] private float jumpPower = 3.0f;
     [SerializeField] private float _speed = 5.0f;
 
     private void Awake() {
@@ -16,7 +19,8 @@ public class PlayerController : MonoBehaviour
     }
 
     private void Update() {
-        _characterController.Move(_direction * _speed * Time.deltaTime);
+        ApplyMovement();
+        ApplyGravity();
     }
 
     public void Move(InputAction.CallbackContext context)
@@ -24,4 +28,30 @@ public class PlayerController : MonoBehaviour
         _input = context.ReadValue<Vector2>();
         _direction = new Vector3(_input.x, 0.0f, _input.y);
     }
+
+    private void ApplyGravity()
+    {
+        if (IsGrounded() && _velocity < 0.0f)
+        {
+            _velocity=-1.0f;
+        }
+        else{
+            _velocity += _gravity * gravityMultiplier * Time.deltaTime;
+        }
+        _direction.y = _velocity;
+    }
+    private void ApplyMovement()
+    {
+        _characterController.Move(_direction * _speed * Time.deltaTime);
+    }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if (!context.started) return;
+        if (!IsGrounded()) return;
+
+        _velocity = jumpPower;
+    }
+
+    private bool IsGrounded() => _characterController.isGrounded;
 }
