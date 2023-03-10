950c413f5787fe66e8a9f23e336b3e0b0af2f483
folecrom <90184290+folecrom@users.noreply.github.com>
2023-03-10 16:59:51 +0100

    correction

--- Parkour Game/Assets/script/fpc.cs
+++ Parkour Game/Assets/script/fpc.cs
@@ -87,37 +87,8 @@ public class fpc : MonoBehaviour
         }
         else if (context.canceled)
         {
-            _speed = 5.0f;
+            _speed = 2.0f;
         }
     }
-    public float slideSpeed = 5f; // vitesse de glissement
-    public float slideDuration = 1f; // durée de glissement
-    private float slideTime = 0f; // temps de glissement écoulé
-    private bool isSliding = false; // est-ce que le personnage est en train de glisser
-
-    public void Slide(InputAction.CallbackContext context)
-    {
-       /* if (Input.GetKeyDown(KeyCode.LeftControl)) // si la touche de glissement est enfoncée
-        {
-            if (!isSliding) // si le personnage n'est pas déjà en train de glisser
-            {
-                isSliding = true;
-                slideTime = 0f;
-            }
-        }
-
-        if (isSliding) // si le personnage est en train de glisser
-        {
-            slideTime += Time.deltaTime;
-            if (slideTime > slideDuration) // si la durée de glissement est écoulée
-            {
-                isSliding = false;
-            }
-            else // sinon, faire glisser le personnage
-            {
-                controller.Move(transform.forward * slideSpeed * Time.deltaTime);
-            }
-        }
-   */ }
     private bool IsGrounded() => _characterController.isGrounded;
 }
