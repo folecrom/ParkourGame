e310d10aad7c4dc3c6f009a17237946f54883a3f
folecrom <90184290+folecrom@users.noreply.github.com>
2023-04-12 14:28:22 +0200

    update menu

---
+++ Parkour Game/Assets/script/GameManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance { get; private set; }
+    
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(this);
+            return;
+        }
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+    }
+    
+    public void ChangeScene(string _sceneName)
+    {
+        SceneManager.LoadScene(_sceneName);
+    }
+    public void QuitGame()
+    {
+        Debug.Log("QUIT!");
+        Application.Quit();
+    }
+};
+
