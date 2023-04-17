fb59d6729ec5e7f6bf4f934439ae11369c077336
folecrom <90184290+folecrom@users.noreply.github.com>
2023-04-17 17:20:47 +0200

    affichage meilleur temps sur le tuto et le niveau 1

--- Parkour Game/Assets/script/result.cs
+++ Parkour Game/Assets/script/result.cs
@@ -5,6 +5,8 @@ public class result : MonoBehaviour
 {
     public Text resultText;
     private float bestTime;
+    public Text resultlevel2;
+    private float bestTime2;
 
     private void Start()
     {
@@ -12,5 +14,8 @@ public class result : MonoBehaviour
         bestTime = PlayerPrefs.GetFloat("BestTime");
         resultText.text = "best time :" + bestTime.ToString("F2") + " secondes";
         resultText.color = Color.green;
+        bestTime2 = PlayerPrefs.GetFloat("BestTime2");
+        resultlevel2.text = "best time :" + bestTime2.ToString("F2") + " secondes";
+        resultlevel2.color = Color.green;
     }
 }
