9cfcee83b5a97ce0300c97d1f853730b72b66d00
Med Ars <mehdi.arroussi@ynov.com>
2023-04-25 13:07:44 +0200

    push

--- Parkour Game/Assets/script/joueur/Cameraplayer.cs
+++ Parkour Game/Assets/script/joueur/Cameraplayer.cs
@@ -1,61 +1,61 @@
-// using System;
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
-// using UnityEngine.InputSystem;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
 
-// [RequireComponent(typeof(CharacterController))]
-// public class Cameraplayer : MonoBehaviour
-
-
-
-
-
-
-/////******* 1st VERSION ==> Character Controller ******//////
+[RequireComponent(typeof(CharacterController))]
+public class Cameraplayer : MonoBehaviour
 
 
 
 
 
 
-// {
-//     public float sensitivity = 100f;
-//     public Transform playerBody;
+///******* 1st VERSION ==> Character Controller ******//////
+
+
+
+
+
+
+{
+    public float sensitivity = 100f;
+    public Transform playerBody;
     
-//     float mouseX ;
-//     float mouseY ;
+    float mouseX ;
+    float mouseY ;
 
-//     //float xRotation = 0f;
-//     Vector2 _input;
-//     Vector3 _direction;
+    //float xRotation = 0f;
+    Vector2 _input;
+    Vector3 _direction;
 
-//     void Start()
-//     {
+    void Start()
+    {
         
-//     }
+    }
 
-//     void Update()
-//     {
-//          mouseX = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
-//          mouseY = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+    void Update()
+    {
+         mouseX = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+         mouseY = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
 
-//         mouseY = Mathf.Clamp(mouseY, -90f, 90f);
+        mouseY = Mathf.Clamp(mouseY, -90f, 90f);
 
-//         transform.localRotation = Quaternion.Euler(mouseX, 0f, mouseY);
-//         playerBody.Rotate(Vector3.up * mouseY);
-//     }
+        transform.localRotation = Quaternion.Euler(mouseX, 0f, mouseY);
+        playerBody.Rotate(Vector3.up * mouseY);
+    }
 
-//     void FixedUpdate()
-//     {
-//         _direction = new Vector3(_input.x, 0.0f, _input.y);
-//         _direction = transform.TransformDirection(_direction);
+    void FixedUpdate()
+    {
+        _direction = new Vector3(_input.x, 0.0f, _input.y);
+        _direction = transform.TransformDirection(_direction);
 
-//         playerBody.transform.position += _direction;
-//     }
+        playerBody.transform.position += _direction;
+    }
 
-//     public void Move(InputAction.CallbackContext context)
-//     {
-//         _input = context.ReadValue<Vector2>();
-//     }
-// }
+    public void Move(InputAction.CallbackContext context)
+    {
+        _input = context.ReadValue<Vector2>();
+    }
+}
--- Parkour Game/Assets/script/joueur/Grip.cs
+++ Parkour Game/Assets/script/joueur/Grip.cs
@@ -1,138 +1,138 @@
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-// public class Grip : MonoBehaviour {
-//     [Header("References")]
-//     public fpc pm;
-//     public Transform orientation;
-//     public Transform cam;
-//     public Rigidbody rb;
+public class Grip : MonoBehaviour {
+    [Header("References")]
+    public fpc pm;
+    public Transform orientation;
+    public Transform cam;
+    public Rigidbody rb;
 
-//     [Header("Grip Grabbing")]
-//     public float moveToGripSpeed;
-//     public float maxGripGrabDistance;
-//     public float minTimeOnGrip;
-//     private float timeOnGrip;
-//     public bool holdGrip;
+    [Header("Grip Grabbing")]
+    public float moveToGripSpeed;
+    public float maxGripGrabDistance;
+    public float minTimeOnGrip;
+    private float timeOnGrip;
+    public bool holdGrip;
 
-//     [Header("Grip Jumping")]
-//     public KeyCode jumpKey = KeyCode.Space;
-//     public float gripJumpForwardForce;
-//     public float gripJumpUpwardForce;
+    [Header("Grip Jumping")]
+    public KeyCode jumpKey = KeyCode.Space;
+    public float gripJumpForwardForce;
+    public float gripJumpUpwardForce;
 
-//     [Header("Grip Detection")]
-//     public float gripDetectionLength;
-//     public float gripSphereCastRadius;
-//     public LayerMask WhatIsGrip;
-//     private Transform lastGrip;
-//     private Transform currentGrip;
-//     private RaycastHit gripHit;
+    [Header("Grip Detection")]
+    public float gripDetectionLength;
+    public float gripSphereCastRadius;
+    public LayerMask WhatIsGrip;
+    private Transform lastGrip;
+    private Transform currentGrip;
+    private RaycastHit gripHit;
 
-//     [Header("Exiting")]
-//     public bool exitingGrip;
-//     public float exitGripTime;
-//     public float exitGripTimer;
+    [Header("Exiting")]
+    public bool exitingGrip;
+    public float exitGripTime;
+    public float exitGripTimer;
 
-//     private void start() {
-//         rb = GetComponent<Rigidbody>();
-//         pm = GetComponent<fpc>();
-//     }
-//     private void Update() {
-//         GripDetection();
-//         StateMachine();
-//         FreezeBodyOnGrip();
-//     }
+    private void start() {
+        rb = GetComponent<Rigidbody>();
+        pm = GetComponent<fpc>();
+    }
+    private void Update() {
+        GripDetection();
+        StateMachine();
+        FreezeBodyOnGrip();
+    }
 
-//     private void StateMachine() {
-//         float horizontalInput = Input.GetAxisRaw("Horizontal");
-//         float verticalInput = Input.GetAxisRaw("Vertical");
-//         bool KeyInputPressed = horizontalInput != 0 || verticalInput != 0!;
+    private void StateMachine() {
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        float verticalInput = Input.GetAxisRaw("Vertical");
+        bool KeyInputPressed = horizontalInput != 0 || verticalInput != 0!;
 
-//         /// 1st State : Holding on a grip
-//         if(holdGrip)
-//             {
-//                 FreezeBodyOnGrip();
+        /// 1st State : Holding on a grip
+        if(holdGrip)
+            {
+                FreezeBodyOnGrip();
 
-//                 timeOnGrip += Time.deltaTime;
-//                 if(timeOnGrip > minTimeOnGrip && KeyInputPressed) EnterGripHold();
+                timeOnGrip += Time.deltaTime;
+                if(timeOnGrip > minTimeOnGrip && KeyInputPressed) EnterGripHold();
 
-//                 if(Input.GetKeyDown(jumpKey)) GripJump();
-//             }
-//         /// 2nd State : Exiting a grip
-//         else if(exitingGrip) {
-//             if(exitGripTimer > 0) exitGripTimer -= Time.deltaTime;
-//             else { exitingGrip = false;}
-//         }
-//     }
+                if(Input.GetKeyDown(jumpKey)) GripJump();
+            }
+        /// 2nd State : Exiting a grip
+        else if(exitingGrip) {
+            if(exitGripTimer > 0) exitGripTimer -= Time.deltaTime;
+            else { exitingGrip = false;}
+        }
+    }
 
 
-//     private void GripDetection() {
-//         bool gripDetected = Physics.SphereCast(transform.position, gripSphereCastRadius, cam.forward, out gripHit, gripDetectionLength, WhatIsGrip);
-//         if(!gripDetected) return;
+    private void GripDetection() {
+        bool gripDetected = Physics.SphereCast(transform.position, gripSphereCastRadius, cam.forward, out gripHit, gripDetectionLength, WhatIsGrip);
+        if(!gripDetected) return;
         
-//         float distanceToGrip = Vector3.Distance(transform.position, gripHit.transform.position);
-//         if (gripHit.transform == lastGrip) return;
+        float distanceToGrip = Vector3.Distance(transform.position, gripHit.transform.position);
+        if (gripHit.transform == lastGrip) return;
 
-//         if(distanceToGrip < maxGripGrabDistance && !holdGrip) EnterGripHold();
+        if(distanceToGrip < maxGripGrabDistance && !holdGrip) EnterGripHold();
 
-//     }
-//     private void GripJump() {
-//         ExitGripHold();
-//         Invoke(nameof(DelayedJumpForce), 0.05f);
-//     }
-//     private void DelayedJumpForce() {
-//         Vector3 forceToAdd = cam.forward * gripJumpForwardForce + orientation.up * gripJumpUpwardForce;
-//         rb.velocity = Vector3.zero;
-//         rb.AddForce(forceToAdd, ForceMode.Impulse);
-//     }
-//     private void EnterGripHold() {
-//         holdGrip = true;
-//         pm.unlimited = true;
-//         pm.restricted = true;
-//         currentGrip = gripHit.transform;
-//         lastGrip = gripHit.transform;
+    }
+    private void GripJump() {
+        ExitGripHold();
+        Invoke(nameof(DelayedJumpForce), 0.05f);
+    }
+    private void DelayedJumpForce() {
+        Vector3 forceToAdd = cam.forward * gripJumpForwardForce + orientation.up * gripJumpUpwardForce;
+        rb.velocity = Vector3.zero;
+        rb.AddForce(forceToAdd, ForceMode.Impulse);
+    }
+    private void EnterGripHold() {
+        holdGrip = true;
+        pm.unlimited = true;
+        pm.restricted = true;
+        currentGrip = gripHit.transform;
+        lastGrip = gripHit.transform;
         
-//         rb.useGravity = false;
-//         rb.velocity = Vector3.zero;
-//     }
-//     private void FreezeBodyOnGrip() {
-//         rb.useGravity = false;
+        rb.useGravity = false;
+        rb.velocity = Vector3.zero;
+    }
+    private void FreezeBodyOnGrip() {
+        rb.useGravity = false;
 
-//         Vector3 directionToGrip = currentGrip.position - transform.position;
-//         float distanceToGrip = Vector3.Distance(transform.position, currentGrip.position);
-//         /// Grab player towards the grip
-//         if(distanceToGrip > 1f){
-//             if(rb.velocity.magnitude < moveToGripSpeed) {
-//                 rb.AddForce(directionToGrip.normalized * moveToGripSpeed * 1000f * Time.deltaTime);
-//             }
-//         }
-//         /// Holding player on grip
-//         else {
-//             if(!pm.freeze) pm.freeze = true;
-//             if(pm.unlimited) pm.unlimited = false;
-//         }
-//         /// Exiting if an issue happens
-//         if(distanceToGrip > maxGripGrabDistance) ExitGripHold();
-//     }
-//     private void ExitGripHold() {
-//         holdGrip = false;
-//         pm.restricted = false;
-//         timeOnGrip = 0f;
-//         pm.freeze = false;
-//         rb.useGravity = true;
-//         exitingGrip = true;
-//         exitGripTimer = exitGripTime;
+        Vector3 directionToGrip = currentGrip.position - transform.position;
+        float distanceToGrip = Vector3.Distance(transform.position, currentGrip.position);
+        /// Grab player towards the grip
+        if(distanceToGrip > 1f){
+            if(rb.velocity.magnitude < moveToGripSpeed) {
+                rb.AddForce(directionToGrip.normalized * moveToGripSpeed * 1000f * Time.deltaTime);
+            }
+        }
+        /// Holding player on grip
+        else {
+            if(!pm.freeze) pm.freeze = true;
+            if(pm.unlimited) pm.unlimited = false;
+        }
+        /// Exiting if an issue happens
+        if(distanceToGrip > maxGripGrabDistance) ExitGripHold();
+    }
+    private void ExitGripHold() {
+        holdGrip = false;
+        pm.restricted = false;
+        timeOnGrip = 0f;
+        pm.freeze = false;
+        rb.useGravity = true;
+        exitingGrip = true;
+        exitGripTimer = exitGripTime;
 
-//         StopAllCoroutines();
-//         Invoke(nameof(ResetLastGrip), 1f);
-//     }
-//     private void ResetLastGrip() {
-//         lastGrip = null;
-//     }
-
-
+        StopAllCoroutines();
+        Invoke(nameof(ResetLastGrip), 1f);
+    }
+    private void ResetLastGrip() {
+        lastGrip = null;
+    }
 
 
 
-// }
+
+
+}
